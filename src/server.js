c8166f837a10e6547530e0cb9836a81d160c7377
in100tiva <projetoin100tiva@gmail.com>
2025-05-23 16:46:39 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,20 +2,34 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer( async (req, res) => {
     const {method, url} = req
- 
-    if (method === "GET" && url ==='/users') {
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
+
+    if (method === "GET" && url === '/users') {
         return res
-        .setHeader('Content-Type', 'Application/json')
+        .setHeader('Content-Type', 'application/json')
         .end(JSON.stringify(users))
     }
 
-    if (method === 'POST' && url === '/users') {
+    if (method === "POST" && url === '/users') {
+        const { name, email } = req.body
+
         users.push({
             id: 1,
-            nome: 'Luan Oliveira',
-            email: 'LuanPDD@hotmail.com',
+            name,
+            email,
         })
 
         return res.writeHead(201).end()
@@ -24,4 +38,4 @@ const server = http.createServer((req, res) => {
     return res.writeHead(404).end()
 })
 
-server.listen(3333);
+server.listen(3333)
